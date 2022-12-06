5067413f263d234e644c26f6f81b7cdcd5249389
benNjogu <kamandemachafi@gmail.com>
2022-12-06 20:55:03 +0300

    Creating a basic route

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express from "express";
 
 const app = express();
-app.listen(8000, () => console.log("Server started at"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world.");
+});
+
+app.listen(8000, () => console.log("Server started."));
